4ffe9949ffd3e91bd4e0c63ca1ce51e2c871bb1d
Lewis Haley <lewis.haley@youview.com>
2023-03-11 23:46:28 +0000

    Extract all CLI options
    Previously, the prototype version of the tool had potential "options"
    hard-coded in the code. This commit moves them to proper CLI options.

--- src/mushmallow/__main__.py
+++ src/mushmallow/__main__.py
@@ -18,13 +18,44 @@ def parse_args():
         type=pathlib.Path,
         help="The file containing Marshmallow schemas to format and/or fix",
     )
+    parser.add_argument(
+        "--max-line-length",
+        default=80,
+        type=int,
+        help="How many characters per line to allow",
+    )
+    parser.add_argument(
+        "--indent-size",
+        default=4,
+        type=int,
+        help="The number of spaces per indent",
+    )
+    parser.add_argument(
+        "--fix-kwargs-for-marshmallow-4",
+        action="store_true",
+        help="If True, convert kwarg fields to metadata fields as per Marshmallow 4",
+    )
+    parser.add_argument(
+        "--sort",
+        action="store_true",
+        help=(
+            "Sort kwarg and/or metadata fields alphabetically. Otherwise "
+            "ordering is arbitrary."
+        ),
+    )
     return parser.parse_args()
 
 
 def main():
     """Mushmallow CLI entry-point."""
     args = parse_args()
-    fix_marshmallow(args.file)
+    fix_marshmallow(
+        args.file,
+        max_line_length=args.max_line_length,
+        indent_size=args.indent_size,
+        fix_kwargs_for_marshmallow_4=args.fix_kwargs_for_marshmallow_4,
+        sort=args.sort,
+    )
 
 
 if __name__ == "__main__":
